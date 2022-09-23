1bab4816bd15568ff1e3378d87c8332d0c9ad6e9
karunasagar555 <skarunasagar3@gmail.com>
2022-09-23 15:52:21 +0530

    printZ pattern

---
+++ Getting_Started/printZ.java
@@ -0,0 +1,40 @@
+/* Q) Print Z
+Easy
+
+1. You are required to print a 'z' of size 5 using '*'.
+
+Constraints
+No Constraints
+
+Format
+Input
+There is no input
+
+Example
+Sample Input
+
+No Input
+
+Sample Output
+*****
+   *
+  *
+ *
+*****
+*/
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) 
+    {
+        // Write your code here
+         System.out.println("*****");
+		 System.out.println("   *");
+		 System.out.println("  *");   
+		 System.out.println(" *");      
+		 System.out.println("*****");
+
+    }
+}
